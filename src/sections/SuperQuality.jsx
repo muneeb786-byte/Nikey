ea04af737907581388d74c1cce37fdef14fd12fd
muneeb786-byte <muneebrahman28185@gmail.com>
2025-08-26 12:38:23 -0700

    Add files via upload

---
+++ src/sections/SuperQuality.jsx
@@ -0,0 +1,37 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
+const SuperQuality = () => {
+  return (
+    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container ">
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl capitalize  font-bold lg:max-w-lg"> We provide you
+          <span className="text-coral-red"> Super </span>
+          <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meiculously crafted footwear is designed to elevate your experience, providing
+          you unmatched, quallity , innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Discover Stylish Nike arrivals, quality  comfort,
+          and innovation for yyourr active life
+        </p>
+        <div className="mt-11">
+          <Button label="Viiew details" />
+        </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items-center ">
+        <img src={shoe8} alt="shoe8" 
+        width={570}
+        height={522}
+        className="object-contain"
+        />
+
+      </div>
+    </section>
+  )
+};
+
+export default SuperQuality;
